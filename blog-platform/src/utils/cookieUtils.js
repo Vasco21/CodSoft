699e37abd9097693c789ffbcff58de0fa9e86607
Vasco21 <vascoeti123@gmail.com>
2024-12-03 05:50:37 +0200

    all

--- blog-platform/src/utils/cookieUtils.js
+++ blog-platform/src/utils/cookieUtils.js
@@ -1,6 +1,6 @@
 // Set a first-party cookie to store user preferences
 export function setPreference(name, value) {
-    document.cookie = `${name}=${value}; path=/; max-age=31536000`; // 1 year expiration
+    document.cookie = `${name}=${value}; path=/; max-age=31536000`; 
   }
   
   // Get a first-party cookie
